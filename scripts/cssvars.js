7bf23461dba6ca709bac6c5c555453973b72ac5c
Vidar Eldøy <vidar@eldoy.com>
2021-01-13 00:55:13 +0100

    Create css var script

--- scripts/cssvars.js
+++ scripts/cssvars.js
@@ -1,11 +1,18 @@
 const fs = require('fs')
+const path = require('path')
+
+const root = process.cwd()
 
 async function run() {
   const files = fs.readdirSync('scss')
   for (const name of files) {
-    if (name.startsWith('_')) {
-      const file = fs.readFileSync(name, 'utf8')
-      console.log(file)
+    if (name.startsWith('_') && !name.startsWith('_variables')) {
+      const abs = path.join(root, 'scss', name)
+      let file = fs.readFileSync(abs, 'utf8')
+      file = file.replace(/\$([a-z0-9-]+)/g, function(match, p1) {
+        return `var(--${p1}, ${match})`
+      })
+      fs.writeFileSync(abs, file)
     }
   }
 }
